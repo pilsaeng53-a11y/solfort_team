2643141fba433e2f86bef9ae295fc1e39f77f18d
pilsaeng53-a11y <pilsaeng53@gmail.com>
2026-04-09 01:52:15 +0900

    Convert: src/pages/Setup.jsx to Neon DB

--- src/pages/Setup.jsx
+++ src/pages/Setup.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { base44 } from "@/api/base44Client";
+import { base44 } from "@/api/neonClient";
 import { useNavigate } from "react-router-dom";
 import SFLogo from "../components/SFLogo";
 import SFCard from "../components/SFCard";
